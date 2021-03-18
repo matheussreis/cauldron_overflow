bdd548dec5cc69920098bf975dcbeeb8bd21c045
Matheus Reis <matheusdsvreis@gmail.com>
2021-03-18 10:18:08 +0000

    Webpack Encore: JavaScript Greatness

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,19 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+// After run "yarn add jquery", write the import bellow
+import $ from 'jquery';
+
+/* Simple (ugly) code to handle the comment vote up/down*/
+var $container = $('.js-vote-arrows');
+$container.find('a').on('click', function (e) {
+    e.preventDefault();
+    var $link = $(e.currentTarget);
+    $.ajax({
+        url: '/comments/10/vote/' + $link.data('direction'),
+        method: 'POST'
+    }).then(function (response) {
+        $container.find('.js-vote-total').text(response.votes);
+    });
+});
